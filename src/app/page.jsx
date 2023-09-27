d7149cd544eae0cbcfc95befd01f9dca1dd13d89
hebertdev82 <hebertdev82@gmail.com>
2023-09-27 18:53:16 -0300

    formulario

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,46 @@
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../config/firebase'
 import Image from "next/image";
 import uaitrampo from "../../public/uaiTrampo.png"
 
-export default function Home() {
+function ItemCadastro() {
+  const [nome, setNome] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [email, setEmail] = useState("");
+  const [segmento, setSegmento] = useState("");
+
+  const handleNomeChange = (e) => { setNome(e.target.value); };
+  const handleCidadeChange = (e) => { setCidade(e.target.value); };
+  const handleTelefoneChange = (e) => { setTelefone(e.target.value); };
+  const handleEmailChange = (e) => { setEmail(e.target.value); };
+  const handleSegmentoChange = (e) => { setSegmento(e.target.value); };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "profissionais"), {
+        nome: nome,
+        cidade: cidade,
+        telefone: telefone,
+        email: email,
+        segmento: segmento
+      });
+
+      setNome("");
+      setCidade("");
+      setTelefone("");
+      setEmail("");
+      setSegmento("");
+
+      alert("Cadastro realizado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao cadastrar:", error);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-center items-center pt-20">
@@ -16,20 +55,29 @@ export default function Home() {
         </h2>
       </div>
       <div className="flex justify-center">
-        <form className="flex flex-col justify-center bg-gradient-to-b from-laranjaclaro to-laranja w-96 p-5 rounded-xl">
+        <form className="flex flex-col justify-center bg-gradient-to-b from-laranjaclaro to-laranja w-96 p-5 rounded-xl" onSubmit={handleSubmit}>
+
           <label htmlFor="nome" className="text-branco font-bold">Nome:</label>
-          <input type="text" className="px-2 rounded"/>
+          <input type="text" className="px-2 rounded" value={nome} onChange={handleNomeChange} />
+          
           <label htmlFor="Cidade" className="text-branco font-bold">Cidade:</label>
-          <input type="text" className="px-2 rounded"/>
+          <input type="text" className="px-2 rounded" value={cidade} onChange={handleCidadeChange} />
+          
           <label htmlFor="Telefone" className="text-branco font-bold">Telefone:</label>
-          <input type="text" className="px-2 rounded"/>
+          <input type="text" className="px-2 rounded" value={telefone} onChange={handleTelefoneChange} />
+          
           <label htmlFor="email" className="text-branco font-bold">Email:</label>
-          <input type="text" className="px-2 rounded"/>
+          <input type="text" className="px-2 rounded" value={email} onChange={handleEmailChange} />
+          
           <label htmlFor="seguimento" className="text-branco font-bold">Segmento:</label>
-          <input type="text" className="px-2 rounded"/>
-          <button className="bg-laranjaclaro w-44 mt-5 p-2 text-xl font-bold text-branco hover:bg-branco hover:text-laranja transition-all duration-500 rounded-xl">Cadastrar</button>
+          <input type="text" className="px-2 rounded" value={segmento} onChange={handleSegmentoChange} />
+          
+          <button className="bg-laranjaclaro w-44 mt-5 p-2 text-xl font-bold text-branco hover:bg-branco hover:text-laranja transition-all duration-500 rounded-xl" type="submit">Cadastrar</button>
+
         </form>
       </div>
     </div>
   )
 }
+
+export default ItemCadastro;
---
+++ src/config/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVAM3mJqleaDixwmjqC-LWs2DOjOIGfMY",
+  authDomain: "uaitrampo-14814.firebaseapp.com",
+  projectId: "uaitrampo-14814",
+  storageBucket: "uaitrampo-14814.appspot.com",
+  messagingSenderId: "332649654619",
+  appId: "1:332649654619:web:a3b55fba562918f41c527e"
+};
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+const db = getFirestore(app)
+export {db}
